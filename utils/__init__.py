80cbbb9029a341539cfc58459da8c536423b6ac9
star14ms <68639278+star14ms@users.noreply.github.com>
2023-01-06 22:28:34 +0900

    Fix: 없어진 함수 불러오지 않기

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,3 @@
-from .common import read_urls_info, save_text_list, save_image, Time
+from .common import save_text_list, save_image, Time
 from .print import print_html, width_str, rstrip_until_max_width, pad_spaces
 from .visualize import show_image, show_images, plot_frequency_chart, draw_word_cloud
